55924da5a250676ef2852891294d51ab1f7b8539
developerC76 <jsb78241@gmail.com>
2025-01-14 22:46:54 -0500

    matrix inverse for 3x3 complete btw internte exmaples contain mistakes...

--- matrix.py
+++ matrix.py
@@ -1,62 +1,92 @@
 #!/usr/bin/env python3
 
+
 def printmat(mat):
-    for i in mat: 
+    for i in mat:
         print(i)
 
-def det2x2(mat): 
-    return mat[0][0]*mat[1][1] - mat[0][1]*mat[1][0]
 
-def cofactor(mat): 
+def det2x2(mat):
+    return mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0]
+
+
+def adj(cofactor):
+    adj = []
+    for i in range(len(cofactor)):
+        adj.append([])
+        for j in range(len(cofactor)):
+            adj[i].append(cofactor[j][i])
+    return adj
+
+
+def cofactor(mat):
     cofactor = [[], [], []]
-    x = [[mat[1][1], mat[1][2]], [mat[2][1], mat[2][2]]]
-    cofactor[0].append(det2x2(x))
-    x = [[mat[1][0], mat[1][2]], [mat[2][0], mat[2][2]]]
-    cofactor[0].append(det2x2(x))
-    x = [[mat[1][0], mat[1][1]], [mat[2][0], mat[2][1]]]
-    cofactor[0].append(det2x2(x))
-    
-    x = [[mat[0][1], mat[0][2]], [mat[2][1], mat[2][2]]]
-    cofactor[1].append(det2x2(x))
-    x = [[mat[0][0], mat[0][2]], [mat[2][0], mat[2][2]]]
-    cofactor[1].append(det2x2(x))
-    x = [[mat[0][0], mat[0][1]], [mat[2][0], mat[2][1]]]
-    cofactor[1].append(det2x2(x))
-    
-    x = [[mat[0][1], mat[0][2]], [mat[1][1], mat[1][2]]]
-    cofactor[0].append(det2x2(x))
-    x = [[mat[0][0], mat[0][2]], [mat[1][0], mat[1][2]]]
-    cofactor[0].append(det2x2(x))
-    x = [[mat[0][0], mat[0][1]], [mat[1][0], mat[1][1]]]
-    cofactor[0].append(det2x2(x))
-    
+    cofactor[0].append(det2x2([[mat[1][1], mat[1][2]], [mat[2][1], mat[2][2]]]))
+    cofactor[0].append(-det2x2([[mat[1][0], mat[1][2]], [mat[2][0], mat[2][2]]]))
+    cofactor[0].append(det2x2([[mat[1][0], mat[1][1]], [mat[2][0], mat[2][1]]]))
+
+    cofactor[1].append(-det2x2([[mat[0][1], mat[0][2]], [mat[2][1], mat[2][2]]]))
+    cofactor[1].append(det2x2([[mat[0][0], mat[0][2]], [mat[2][0], mat[2][2]]]))
+    cofactor[1].append(-det2x2([[mat[0][0], mat[0][1]], [mat[2][0], mat[2][1]]]))
+
+    cofactor[2].append(det2x2([[mat[0][1], mat[0][2]], [mat[1][1], mat[1][2]]]))
+    cofactor[2].append(-det2x2([[mat[0][0], mat[0][2]], [mat[1][0], mat[1][2]]]))
+    cofactor[2].append(det2x2([[mat[0][0], mat[0][1]], [mat[1][0], mat[1][1]]]))
+
+    return cofactor
 
 
- 
-    
+def det3x3(mat):
+    cof = cofactor(mat)
+    print(cof)
+    return mat[0][0] * cof[0][0] - mat[0][1] * cof[0][1] + mat[0][2] * cof[0][2]
 
 
-def inv2x2(mat): 
-    inv = []
-    for i in range(len(mat)): 
-        inv.append([])
-        for j in range(len(mat)): 
-            inv[i].append(mat[i][j]*(1/det2x2(mat)))
+def inv2x2(mat):
+    det = det2x2(mat)
+    if det == 0:
+        return "Matrix is singular (no inverse)"
+    inv = [[mat[1][1] / det, -mat[0][1] / det], [-mat[1][0] / det, mat[0][0] / det]]
     return inv
 
-print("Matrix Inverse")
-size = int(input("Number of rows in square matrix: "))
-mat = []
-for i in range(size): 
-    mat.append([])
-    for j in range(size): 
-        num = int(input(f"A{i+1}{j+1}: "))
-        mat[i].append(num)
 
-print("Original Matrix: ")
-printmat(mat)
-print("Inverse: ")
-if size == 2: 
-    printmat(inv2x2(mat))
-else: 
-    printmat(inv3x3(mat))
+def inv3x3(mat):
+    det = det3x3(mat)
+    if det == 0:
+        return "Matrix is singular (no inverse)"
+    print(det)
+    adj_mat = adj(cofactor(mat))
+    print(adj_mat)
+    inv = []
+    for i in range(3):
+        inv.append([adj_mat[i][0] / det, adj_mat[i][1] / det, adj_mat[i][2] / det])
+
+    return inv
+
+
+if __name__ == "__main__":
+    print("Matrix Inverse")
+    size = int(input("Number of rows in square matrix (2 or 3): "))
+    mat = []
+    for i in range(size):
+        mat.append([])
+        for j in range(size):
+            num = int(input(f"A{i+1}{j+1}: "))
+            mat[i].append(num)
+
+    print("Original Matrix: ")
+    printmat(mat)
+
+    if size == 2:
+        inverse = inv2x2(mat)
+    elif size == 3:
+        inverse = inv3x3(mat)
+    else:
+        print("Invalid matrix size. Only 2x2 or 3x3 matrices are supported.")
+        exit()
+
+    if isinstance(inverse, str):  # Check if the result is an error message
+        print("Inverse:", inverse)
+    else:
+        print("Inverse: ")
+        printmat(inverse)
